fa4e7f0d2b07d3cb511849b808a942c5b0fd4e70
senfkorn <40722551+senfkorn@users.noreply.github.com>
2026-05-20 16:09:05 +0200

    Update pcnt_encoder.h

--- components/pcnt_encoder/pcnt_encoder.h
+++ components/pcnt_encoder/pcnt_encoder.h
@@ -10,11 +10,8 @@ class PCNTEncoder : public PollingComponent {
  public:
   PCNTEncoder(gpio_num_t pin_a, gpio_num_t pin_b, float meters_per_pulse);
 
-  Sensor *speed_sensor = nullptr;
-  Sensor *distance_sensor = nullptr;
-
-  void set_speed_sensor(Sensor *s) { speed_sensor = s; }
-  void set_distance_sensor(Sensor *s) { distance_sensor = s; }
+  void set_speed_sensor(Sensor *s) { speed_sensor_ = s; }
+  void set_distance_sensor(Sensor *s) { distance_sensor_ = s; }
 
   void setup() override;
   void update() override;
@@ -25,6 +22,9 @@ class PCNTEncoder : public PollingComponent {
   float meters_per_pulse_;
 
   float total_distance_ = 0;
+
+  Sensor *speed_sensor_ = nullptr;
+  Sensor *distance_sensor_ = nullptr;
 
   pcnt_unit_handle_t unit_;
   pcnt_channel_handle_t channel_;
